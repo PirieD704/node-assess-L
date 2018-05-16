69894e315ec5a5d99cefa9e785b763b695ce90f6
PirieD704 <dp75975@gmail.com>
2018-05-16 19:24:46 -0400

    added fetch and set up simple api call

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const fetch = require('node-fetch');
+const lowesUrl = "http://m.lowes.com/CatalogServices/product/nvalue/v1_0?nValue=4294857975&maxResults=8&offset=0&showURL=1&rollUpVariants=1&showUrl=true&storeNumber=0595&priceFlag=rangeBalance&showMarketingBullets=1"
+
+const getData = async lowesUrl => {
+  try {
+    const response = await fetch(lowesUrl);
+    const json = await response.json();
+    console.log(json.productList);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Interview Test', products: getData(lowesUrl) });
 });
 
 module.exports = router;
