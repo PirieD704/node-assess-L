7eca410c6ed02c446c9a41f69fa65a2aca7161bc
PirieD704 <dp75975@gmail.com>
2018-05-17 23:10:20 -0400

    enabled button and hero functionality. added error handling logic

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,8 +5,15 @@ const lowesUrl = "http://m.lowes.com/CatalogServices/product/nvalue/v1_0?nValue=
 
 
 
-  exports.productsList = async (req, res, next) => {
-    const response = await fetch(lowesUrl);
-    const products = await response.json();
-    res.render('index', { title: 'Interview Test', products: products.productList });
-  };
+exports.productsList = async (req, res, next) => {
+  const response = await fetch(lowesUrl);
+  const products = await response.json();
+  let heroObj = {};
+  if (req.params.id){
+    heroObj = products.productList.find(product => product.pricing.productId == req.params.id)
+  }else{
+    heroObj = products.productList[0]
+  }
+  //const heroObj = products.productList[0];
+  res.render('index', { title: 'Interview Test', products: products.productList,  heroObj });
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
-
+const { catchErrors } = require('../handlers/errorHandlers');
 
 /* GET home page. */
-router.get('/', productController.productsList);
+router.get('/', catchErrors(productController.productsList));
+router.get('/:id', catchErrors(productController.productsList));
 
 module.exports = router;
